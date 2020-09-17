518eb4a5d4fec450411c61dca403a4499309c0ce
Johanna Jansson <joja92@hotmail.com>
2020-09-17 17:17:33 +0200

    Update locationDetails.vue

--- src/components/locationDetails.vue
+++ src/components/locationDetails.vue
@@ -1,23 +1,30 @@
 <template>
-    <ul class="details">
-        <li class="details-item">
-            <span class="details-item-icon">O</span>
-            <p class="details-item-text">{{ detailsData.date }}</p>
-        </li>       
-        <li class="details-item">
-            <span class="details-item-icon">O</span>
-            <p class="details-item-text">{{ detailsData.startTime }} - {{ detailsData.endTime }}</p>
-        </li>
-        <li class="details-item">
-            <span class="details-item-icon">O</span>
-            <p class="details-item-text details-item-place">{{ detailsData.locationPlace }}</p>
-            <p class="details-item-text">{{ detailsData.locationStreet }}, {{ detailsData.locationZip }} {{ detailsData.locationCity }}</p>
-        </li>
-        <li class="details-item">
-            <span class="details-item-icon">O</span>
-            <p class="details-item-text">{{ detailsData.locationNotes }}</p>
-        </li>
-    </ul>
+    <div class="details">
+        <ul class="details-list">
+            <span class="details-line"></span>
+            <li class="details-list-item">
+                <span class="details-list-item-icon"><i class="fas fa-calendar-alt"></i></span>
+                <p class="details-list-item-text">{{ detailsData.date }}</p>
+            </li>       
+            <li class="details-list-item">
+                <span class="details-list-item-icon"><i class="far fa-clock"></i></span>
+                <p class="details-list-item-text">{{ detailsData.startTime }} - {{ detailsData.endTime }}</p>
+            </li>
+            <li class="details-list-item">
+                <span class="details-list-item-icon details-list-item-adress-icon"><i class="fas fa-map-marker-alt"></i></span>
+                <p class="details-list-item-text details-list-item-place">{{ detailsData.locationPlace }}</p>
+                <p class="details-list-item-text">{{ detailsData.locationStreet }}</p>
+                <p class="details-list-item-text">{{ detailsData.locationZip }} {{ detailsData.locationCity }}</p>
+            </li>
+            <li class="details-list-item">
+                <span class="details-list-item-icon"><i class="fas fa-clipboard"></i></span>
+                <p class="details-list-item-text">{{ detailsData.locationNotes }}</p>
+            </li>
+        </ul>
+        <div class="details-map">
+
+        </div>
+    </div>
 </template>
 
 <script>
@@ -44,5 +51,59 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .details {
+        width: 100%;       
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        
+        &-line {
+            width: .5rem;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: 1rem - .25rem;
+            @include gradient(0deg);
+            z-index: -1;
+        }
 
+        &-list {
+            list-style-type: none;
+            width: 100%;
+            position: relative;
+
+            &-item {
+                display: grid;
+                grid-template-columns: 3rem 1fr;
+                margin-bottom: 1.5rem;
+                align-items: center;
+
+                &-icon {
+                    width: 2rem;
+                    height: 2rem;
+                    border-radius: 2rem;
+                    font-size: 1.3rem;
+                    background: $pink;
+                    color: #fff;
+                    @include centeredFlex();
+                    align-self: start;
+                }
+                &-adress-icon {
+                    grid-row: span 3;
+                }
+                &-text {
+                    font-size: 1rem;
+                }
+                &-place {
+                    font-weight: bold;
+                }
+            }
+        }
+
+        &-map {
+            width: 100%;
+            height: 180px;
+            background: #888;
+        }
+    }
 </style>
