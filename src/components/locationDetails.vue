851de8534a1bcc4b7bc4d1d50c06133624e6266c
Johanna Jansson <joja92@hotmail.com>
2020-09-17 15:18:11 +0200

    Update locationDetails.vue

--- src/components/locationDetails.vue
+++ src/components/locationDetails.vue
@@ -1,14 +1,44 @@
 <template>
-    <div class="location">
-        <p class="location-place-text">{{ locationData.locationPlace }}</p>
-    </div>
+    <ul class="details">
+        <li class="details-item">
+            <span class="details-item-icon">O</span>
+            <p class="details-item-text">{{ detailsData.date }}</p>
+        </li>       
+        <li class="details-item">
+            <span class="details-item-icon">O</span>
+            <p class="details-item-text">{{ detailsData.startTime }} - {{ detailsData.endTime }}</p>
+        </li>
+        <li class="details-item">
+            <span class="details-item-icon">O</span>
+            <p class="details-item-text details-item-place">{{ detailsData.locationPlace }}</p>
+            <p class="details-item-text">{{ detailsData.locationStreet }}, {{ detailsData.locationZip }} {{ detailsData.locationCity }}</p>
+        </li>
+        <li class="details-item">
+            <span class="details-item-icon">O</span>
+            <p class="details-item-text">{{ detailsData.locationNotes }}</p>
+        </li>
+    </ul>
 </template>
 
 <script>
 export default {
     name: 'locationDetails',
     props: {
-        locationData: Object
+        //locationData: Object
+    },
+    data() {
+        return {
+            detailsData: {
+                startTime: '12:00',
+                endTime: '15:30',
+                date: 'September 15, 2020',
+                locationPlace: 'Botaniska Trädgården',
+                locationStreet: 'Carl Skottsbergs Gata 22A',
+                locationZip: '413 19',
+                locationCity: 'Gothenburg',
+                locationNotes: 'Vi samlas vid huvudentrén för att sedan röra oss mot japandalen'
+            }
+        }
     }
 }
 </script>
