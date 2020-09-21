b5c32418b8d96a3f6adaf21ef92d3073b91a1f08
Johanna Jansson <joja92@hotmail.com>
2020-09-21 11:42:29 +0200

    Create SearchEvent.vue

---
+++ src/views/SearchEvent.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="search-event">
+        <eventItem 
+            v-for="item in eventsList" 
+            :key="item.eventId" 
+            :eventItem="item"
+        />
+    </section>
+</template>
+
+<script>
+import eventItem from '@/components/eventItem.vue';
+export default {
+    name: 'SearchEvent',
+    components: {
+        eventItem
+    },
+    computed: {
+        eventsList() {
+            return [];
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
