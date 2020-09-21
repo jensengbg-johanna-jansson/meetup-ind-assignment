45bf5db8f3fe57575a61f03deb817ae0cf61895e
Johanna Jansson <joja92@hotmail.com>
2020-09-21 17:42:17 +0200

    Update searchBar.spec.js

--- tests/unit/searchBar.spec.js
+++ tests/unit/searchBar.spec.js
@@ -2,19 +2,21 @@ import { mount } from '@vue/test-utils';
 import searchBar from '@/components/ui-components/searchBar.vue';
 
 describe('searchBar tests', () => {
-    it('should call router with search query on click', async () => {
+    it('should call router with the input value on click', async () => {
         const testQuery = 'test';
         const route = { path: 'search', query: {'term': testQuery}};
         let $router = {
             push: jest.fn()
-        }
-       
+        }      
         let wrapper = mount(searchBar, {
             mocks: {
                 $router,
             },
         });
+        const input =  wrapper.find('input');
 
+        input.element.value = testQuery;
+        await input.trigger('input');
         await wrapper.find('.search-button').trigger('click');          
         
         expect($router.push).toHaveBeenCalledWith(route); 
