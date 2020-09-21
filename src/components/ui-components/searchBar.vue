545f411f5c361bcb6f45c9474924a634e22c5314
Johanna Jansson <joja92@hotmail.com>
2020-09-21 15:43:58 +0200

    Create searchBar.vue

---
+++ src/components/ui-components/searchBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <primaryButton 
+            :buttonText="'go'" 
+            @click.native="startSearch"
+            class="search-button"
+        />
+    </div>
+</template>
+
+<script>
+import primaryButton from '@/components/ui-components/primaryButton.vue';
+export default {
+    name: 'searchBar',
+    components: {
+        primaryButton
+    },
+    methods: {
+        startSearch() {
+            this.$router.push({ path: 'search', query: { term: 'test' }});
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
