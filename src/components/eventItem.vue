1f547ca27e939ffd4b41b744d6d76c51bd75781e
Johanna Jansson <joja92@hotmail.com>
2020-09-21 10:55:18 +0200

    Create eventItem

---
+++ src/components/eventItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="event-item" @click="openEvent">
+
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'eventItem',
+    props: {
+        eventItem: Object
+    },
+    methods: {
+        openEvent() {            
+            this.$router.push('/event/' + this.eventItem.eventId);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
