60966b7ab4b52ac051d6e9e290fa4362ec223cd7
Johanna Jansson <joja92@hotmail.com>
2020-09-22 08:56:42 +0200

    Create Nav.vue

---
+++ src/components/nav.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <button @click="toggleNav">Menu</button>
+        <nav v-if="showNav"></nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'mainNav',
+    data() {
+        return {
+            showNav: false
+        }
+    },
+    methods: {
+        toggleNav() {
+            this.showNav = !this.showNav;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
