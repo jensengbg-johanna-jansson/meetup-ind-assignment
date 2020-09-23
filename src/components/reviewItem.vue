c17fcf19d2cdd555528d77adaf3f7873da47cebb
Johanna Jansson <joja92@hotmail.com>
2020-09-23 14:28:42 +0200

    Create reviewItem.vue

---
+++ src/components/reviewItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <article class="review">
+        <span class="review-user-img"></span>
+        <p class="review-user-name">Sarah Williams</p>
+        <p class="review-text">{{ reviewItem.review }}</p>
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'reviewItem',
+    props: {
+        reviewItem: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .review {
+        @include gradient(0deg);
+        width: 100%;
+        border-radius: 1rem;
+        color: #fff;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        &-text {
+            font-weight: 300;
+            font-size: .8rem;
+            opacity: .8;
+        }
+    }
+</style>
