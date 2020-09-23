fcdbf5bad7e8b9b5e63974735893a6c272b161be
Johanna Jansson <joja92@hotmail.com>
2020-09-23 14:29:52 +0200

    Create reviewList.vue

---
+++ src/components/reviewList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="review-list">
+        <reviewItem 
+            v-for="review in reviewData" 
+            :key="review.reviewId"
+            :reviewItem="review"
+        />
+    </div>
+</template>
+
+<script>
+import reviewItem from '@/components/reviewItem.vue';
+
+export default {
+    name: 'reviewList',
+    components: {
+        reviewItem
+    },
+    props: {
+        reviewData: Array
+    }
+}
+</script>
+
+<style>
+
+</style>
