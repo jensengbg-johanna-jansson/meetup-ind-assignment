b1174b4968e217b824bad9664c07c18a74461bd7
Johanna Jansson <joja92@hotmail.com>
2020-09-24 10:52:53 +0200

    Create filledButton.vue

---
+++ src/components/ui-components/filledButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="filled-button">{{ buttonText }}</button>
+</template>
+
+<script>
+export default {
+    name: 'filledButton',
+    props: {
+        buttonText: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .filled-button {
+        border: none;
+        @include gradient(-90deg);
+        font-family: 'Roboto', sans-serif;
+        color: #fff;
+        text-transform: uppercase;
+        font-weight: 300;
+        font-size: 1.1rem;
+        border-radius: 5rem;
+        padding: .7rem 2rem;
+        box-shadow: 0 0 4px rgba(0, 0, 0, .3);
+    }
+</style>
