1d66bca10188e9127798b8d8731442818dd69c25
Johanna Jansson <joja92@hotmail.com>
2020-09-30 17:57:03 +0200

    Update 404.vue

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,6 +1,7 @@
 <template>
-    <section>
-        <h1>404: Page not found</h1>
+    <section class="error404">
+        <h1 class="error404-header">404</h1>
+        <p>Ooops, could not find page</p>
     </section>
 </template>
 
@@ -10,6 +11,23 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    .error404 {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 90vh;
+        width: 100%;
 
+        &-header {
+            font-size: 10rem;
+            color: $pink;
+            margin-bottom: 1.5rem;
+            line-height: 10rem;
+        }
+        p {
+            font-size: 1.5rem;
+        }
+    }
 </style>
