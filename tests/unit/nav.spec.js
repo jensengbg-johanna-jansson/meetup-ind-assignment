ece06e49e7c079b3189716bd09a06df7a123b920
Johanna Jansson <joja92@hotmail.com>
2020-09-30 15:13:20 +0200

    Update nav.spec.js

--- tests/unit/nav.spec.js
+++ tests/unit/nav.spec.js
@@ -4,7 +4,12 @@ import mainNav from '@/components/nav.vue';
 describe('mainNav tests', () => {
     it('should show menu on click', async () => {
         let wrapper = shallowMount(mainNav, {
-            stubs: ['router-link', 'router-view']
+            stubs: ['router-link', 'router-view'],
+            computed: {
+                isLoggedIn() {
+                    return false
+                }
+            }
         });
         const button =  wrapper.find('button');
 
@@ -15,7 +20,12 @@ describe('mainNav tests', () => {
 
     it('should hide menu if clicked twice', async () => {
         let wrapper = shallowMount(mainNav, {
-            stubs: ['router-link', 'router-view']
+            stubs: ['router-link', 'router-view'],
+            computed: {
+                isLoggedIn() {
+                    return false
+                }
+            }
         });
         const button =  wrapper.find('button');
 
@@ -23,5 +33,33 @@ describe('mainNav tests', () => {
         await button.trigger('click');
         
         expect(wrapper.find('nav').exists()).toBe(false); 
+    })
+
+    it('should show sign in icon if user is not logged in', () => {
+        let wrapper = shallowMount(mainNav, {
+            stubs: ['router-link', 'router-view'],
+            computed: {
+                isLoggedIn() {
+                    return false
+                }
+            }
+        });
+        const buttonExist =  wrapper.find('.menu-sign-in').exists();
+        
+        expect(buttonExist).toBe(true); 
+    })
+
+    it('should show profile icon if user is logged in', () => {
+        let wrapper = shallowMount(mainNav, {
+            stubs: ['router-link', 'router-view'],
+            computed: {
+                isLoggedIn() {
+                    return true
+                }
+            }
+        });
+        const buttonExist =  wrapper.find('.menu-profile').exists();
+        
+        expect(buttonExist).toBe(true); 
     })
 })
